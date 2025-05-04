281a549a7e547614db2e27bd1d94dc943c60ff67
Dhammadeep4 <ddv4001@gmail.com>
2025-05-04 17:58:35 +0530

    removing console log

--- config/mongodb.js
+++ config/mongodb.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   mongoose.connection.on("connected", () => {
-    console.log("DB Connnected✅");
+    //console.log("DB Connnected✅");
   });
 
   await mongoose.connect(`${process.env.MONGODB_URI}/naad-nrutya`);
